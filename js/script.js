e78a9567495bc1be133bccb6bcbafd11ebece6d8
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-10 16:03:02 +0200

    [UPD] create strictur-in-html-with-img

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//REALIZZO LA STRUTTURA DATI//
+
+const profiles = [
+    {
+        nome: 'Wayne Barnett',
+        img: 'wayne-barnett-founder-ceo-jpg',
+        ruolo: 'Founder & CEO'
+    },
+    {
+        nome: 'Angela Caroll',
+        img: 'wayne-barnett-founder-ceo-jpg',
+        ruolo: 'Founder & CEO' 
+    },
+    {
+        nome: 'Walter Gordon',
+        img: 'walter-gordon-office-manager-jpg',
+        ruolo: 'Office Manager' 
+    },
+    {
+        nome: 'Angela Lopez',
+        img: 'angela-lopez-social-media-manager-jpg',
+        ruolo: 'Social Media Manager' 
+    },
+    {
+        nome: 'Scott Estrada',
+        img: 'scott-estrada-developer-jpg',
+        ruolo: 'Developer' 
+    },
+    {
+        nome: 'Barbara Ramos',
+        img: 'barbara-ramos-grapich-design-jpg',
+        ruolo: 'Grapich Designer' 
+    },
+]
